bd3f978d1717f277356a20384e8210b1dc44e4db
Noelle Rolwes <nrolwes@gmail.com>
2018-12-19 11:04:10 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do #CREATE/ reveives form data through params & renders results pg
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+
+    erb :piggied
+  end
+
 end
